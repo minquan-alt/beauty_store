34172887c40c084d67f46d9f168221ddc3857cbc
minquan-alt <gameonlinepa@gmail.com>
2025-03-06 12:33:24 +0700

    maven setting

---
+++ src/main/java/com/beautystore/adeline/AdelineApplication.java
@@ -0,0 +1,13 @@
+package com.beautystore.adeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdelineApplication.class, args);
+	}
+
+}
